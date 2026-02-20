30d54e3f24beb1fc73782dcd123a4e2da9a2f6ca
yaninyzwitty <ianmwa143@gmail.com>
2026-02-20 12:05:42 +0300

    remove redundant fx.Invoke in main handler

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/yaninyzwitty/idempotent-widthrawal-processor/internal/app/di"
-	"go.uber.org/fx"
 )
 
 func main() {
-	app := di.NewFxApp(
-		fx.Invoke(setupSignalHandler),
-	)
+	app := di.NewFxApp()
 
 	app.Run()
-}
-
-func setupSignalHandler(lifecycle fx.Lifecycle, shutdowner fx.Shutdowner) {
-	lifecycle.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			go func() {
-				sigCh := make(chan os.Signal, 1)
-				signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-				<-sigCh
-				_ = shutdowner.Shutdown()
-			}()
-			return nil
-		},
-	})
 }
